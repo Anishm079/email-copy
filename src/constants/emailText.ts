b1cf8956aae784bb2fa8914f94e1e3197b983779
Anishahammad <anish.ahammad@embifi.in>
2024-11-08 12:47:54 +0530

    finished with email copy

--- src/constants/emailText.ts
+++ src/constants/emailText.ts
@@ -1,20 +1,19 @@
+export const copyStyledText = (styledText: HTMLDivElement) => {
+  if (styledText) {
+    const range = document.createRange();
+    range.selectNodeContents(styledText);
+    const selection = window.getSelection();
+    selection?.removeAllRanges(); // Clear any existing selections
+    selection?.addRange(range);
 
-export const copyStyledText = (styledText:HTMLDivElement) => {
-    if (styledText) {
-      const range = document.createRange();
-      range.selectNodeContents(styledText);
-      const selection = window.getSelection();
-      selection?.removeAllRanges(); // Clear any existing selections
-      selection?.addRange(range);
-
-      // Copy the selected content
-      try {
-        document.execCommand('copy');
-      } catch (err) {
-        console.error("Failed to copy:", err);
-      }
-
-      // Cleanup: Deselect the selected content
-      selection?.removeAllRanges();
+    // Copy the selected content
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      console.error('Failed to copy:', err);
     }
-  };
+
+    // Cleanup: Deselect the selected content
+    selection?.removeAllRanges();
+  }
+};
